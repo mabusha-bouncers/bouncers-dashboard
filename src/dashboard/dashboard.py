f36e1b5186b05ad19923cd0ff9284235b23a1aa6
mobius-crypt <mobiusndou@gmail.com>
2021-11-16 10:59:15 +0200

    init dashboard application

---
+++ src/dashboard/dashboard.py
@@ -0,0 +1,15 @@
+
+
+from flask import Blueprint, render_template
+
+dashboard_bp = Blueprint('dashboard', __name__)
+
+
+@dashboard_bp.route('/', methods=['GET', 'POST'])
+def dashboard():
+    """
+
+    :return:
+    """
+    return render_template('index.html')
+
